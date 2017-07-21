1ed80f4692a591a12746cd417b0394e726d97303
mjf55 <mjf55@users.noreply.github.com>
2017-07-21 09:22:29 -0400

    starting Help system

--- myFunctions.py
+++ myFunctions.py
@@ -1,15 +1,16 @@
 import  csv
 from SQLbackend import connect, insert, delete
 import tkMessageBox
+from Tkinter import Tk, Toplevel, Button
 #import os
 
 def notyet(window):
 	tkMessageBox.showinfo(title="Function Not Supported", message='Sorry, I have not implimented this function yet')
 	return
 	
-def myExit(window):
+def myExit(window2close):
 	print "See ya later, Alligator"
-	window.quit()
+	window2close.quit()
 
 def myDelete(status, oid):
 	mDelete = tkMessageBox.askyesno(title="Delete Record", message='Are you sure?')
@@ -115,7 +116,28 @@ def clearEntry(sStatus, END, id, uid1, uid2, pword, pack, used, page9):
 	page9.delete(0,END)
 
 def myInsert(status, END, id, uid1, uid2, pword, pack, used, page9):
-
 	insert(status, uid1.get(), uid2.get(), pword.get(), pack.get(), used.get())
-
 	clearEntry(status, END, id, uid1, uid2, pword, pack, used, page9)
+
+##########################3  Below added for help window ###################
+def raise_window(window):
+    window.attributes('-topmost', 1)
+    window.attributes('-topmost', 0)
+
+# Destroy window
+def destroy_help_window(window2destroy):
+    window2destroy.destroy()
+
+def myHelp():
+# Create new top level window. Opens immediately
+	help_window = Toplevel()
+	help_window.title('da Vinci Tag Manager Help')
+	help_window.geometry("400x200")
+	raise_window(help_window)
+	help_window.focus_force()
+
+	close_button = Button(help_window,  text='Close this help window',  command=lambda: destroy_help_window(help_window)	)
+	close_button.pack()
+
+# help window fcus
+	
